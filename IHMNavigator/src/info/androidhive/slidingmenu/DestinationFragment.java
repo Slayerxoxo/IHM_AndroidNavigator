513e017121551e1052ca39eea99fe3d2afc3e1d4
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-04-04 23:41:49 +0200

    architecture de l'application

---
+++ IHMNavigator/src/info/androidhive/slidingmenu/DestinationFragment.java
@@ -0,0 +1,21 @@
+package info.androidhive.slidingmenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DestinationFragment extends Fragment {
+	
+	public DestinationFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+         
+        return rootView;
+    }
+}
