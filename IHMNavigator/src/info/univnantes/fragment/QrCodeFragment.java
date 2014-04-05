a2e9bd09140bdb9bc1d0ee3d4a1fbabaf54aa564
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-04-05 12:23:37 +0200

    création de la vue qrcode

--- IHMNavigator/src/info/univnantes/fragment/QrCodeFragment.java
+++ IHMNavigator/src/info/univnantes/fragment/QrCodeFragment.java
@@ -15,7 +15,7 @@ public class QrCodeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_pages, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_qrcode, container, false);
          
         return rootView;
     }
