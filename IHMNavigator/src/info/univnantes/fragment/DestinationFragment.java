4008a8ed04071e0b9fb47f80eae6026d7de4b343
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-04-05 13:42:27 +0200

    création vue  dstination et maj icones

--- IHMNavigator/src/info/univnantes/fragment/DestinationFragment.java
+++ IHMNavigator/src/info/univnantes/fragment/DestinationFragment.java
@@ -15,7 +15,7 @@ public class DestinationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_destination, container, false);
          
         return rootView;
     }
