2fe7548e7262f6d7e46c77a1c304e7dd10694626
Slayerxoxo <coraline.marie.cm@gmail.com>
2014-04-05 12:32:01 +0200

    création de la vue déconnexion

--- IHMNavigator/src/info/univnantes/fragment/DeconnexionFragment.java
+++ IHMNavigator/src/info/univnantes/fragment/DeconnexionFragment.java
@@ -16,7 +16,7 @@ public class DeconnexionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_whats_hot, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_deconnexion, container, false);
          
         return rootView;
     }
